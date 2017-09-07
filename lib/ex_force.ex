71053c0e6b5e7ce8912c01cacdd9eec261f779bb
Chulki Lee <chulki.lee@gmail.com>
2017-09-07 11:11:44 -0700

    initial commit

---
+++ lib/ex_force.ex
@@ -0,0 +1,18 @@
+defmodule ExForce do
+  @moduledoc """
+  Documentation for ExForce.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExForce.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_force_test.exs
@@ -0,0 +1,8 @@
+defmodule ExForceTest do
+  use ExUnit.Case
+  doctest ExForce
+
+  test "greets the world" do
+    assert ExForce.hello() == :world
+  end
+end
